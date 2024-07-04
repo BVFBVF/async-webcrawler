0021880c29b8f2fcbee139f9815ac43ba6fb857b
BVFBVF <doubledimas309@gmail.com>
2024-07-05 01:48:48 +0300

    05.07.24 webcrawler-respectful.py

--- webcrawler-respectful.py
+++ webcrawler-respectful.py
@@ -8,7 +8,8 @@ import requests
 import contextlib
 import psycopg2
 import re
-import queue
+import os
+import sys
 
 
 global_urls = []
@@ -16,10 +17,8 @@ processed_urls = []
 error_printed = False
 changedata_flag = False
 last_error = None
-user_input_queue = queue.Queue()
 already_getted_robots_txt = {}
 
-SQL_INSERT_K = 'INSERT INTO keywords (url, keyword) VALUES (%s, %s)'
 
 def check_robots_w_h(url):
     global error_printed, last_error
@@ -128,7 +127,7 @@ def check_robots(url):
     else:
         return already_getted_robots_txt.get(url)
 
-async def get_keywords(url) -> None:
+async def get_keywords(url, SQL_INSERT_K) -> None:
     if url not in processed_urls:
         connection = psycopg2.connect(**db_config)
         cursor = connection.cursor()
@@ -158,7 +157,7 @@ async def get_keywords(url) -> None:
                 last_height = new_height
             kws = driver.find_elements(By.CSS_SELECTOR, '.sc-bTmccw.kpSNBr.MuiTableCell-root.MuiTableCell-body.MuiTableCell-sizeMedium')
             if len(kws) == 0:
-                await get_keywords(url)
+                await get_keywords(url, SQL_INSERT_K)
             for kw in kws:
                 if '$' not in kw.text and kw.text != 'Low' and not re.fullmatch(r'([0-9]+(,)*)+', kw.text) and kw.text != '-' and kw.text != 'High' and kw.text != 'Medium':
                     keywords.append(kw.text)
@@ -170,7 +169,7 @@ async def get_keywords(url) -> None:
                 print('Error while extracting keywords, trying one more time. to learn more about the error, type: "info".')
                 error_printed = True
             last_error = error
-            await get_keywords(url)
+            await get_keywords(url, SQL_INSERT_K)
         finally:
             connection.commit()
             cursor.close()
@@ -217,13 +216,14 @@ async def crawl_w_h(url, processed_urls):
             finally:
                 return results
 
-async def crawl(urls, processed_urls):
+async def crawl(urls, processed_urls, SQL_INSERT_K):
     global error_printed, last_error
     results = []
     for url in urls:
         if url not in processed_urls:
             try:
-                await get_keywords(url)
+                await get_keywords(url, SQL_INSERT_K)
+                print('Keywords for', url, 'were recorded in your database.')
             finally:
                 processed_urls.append(url)
                 while True:
@@ -282,22 +282,19 @@ def reset_flag():
         time.sleep(10)
         error_printed = False
 
-def input_reader():
-    while True:
-        user_input = input()
-        user_input_queue.put(user_input)
-
 def user_input_handler():
     global last_error
     while True:
-        user_input = user_input_queue.get()
+        user_input = sys.stdin.readline().strip()
         if user_input.lower() == 'info' and last_error:
             print(last_error)
         elif user_input.lower() == 'show':
             print(global_urls)
-        elif user_input.lower() == 'terminate':
-            print('Terminating process...')
-            quit(0)
+        elif user_input.lower() == 'stop':
+            print('Stopping process...')
+            os._exit(0)
+        elif user_input.lower() == 'help':
+            print('To learn last error: print - "info"\nTo stop the programm: print - "stop"\nTo check last founded urls: print - "show"')
 
 def get_http(url):
     if not url.startswith('http://') and not url.startswith('https://'):
@@ -327,14 +324,15 @@ skip_extensions = [
     '.svg'
 ]
 
-async def main():
+async def main(SQL_INSERT_K):
+    print('Program started.')
+    global global_urls, table
     rslts = []
-    global global_urls
     filtered_results = []
     while True:
         tasks = []
         for url in global_urls:
-            tasks.append(crawl([url], processed_urls))
+            tasks.append(crawl([url], processed_urls, SQL_INSERT_K))
         global_urls.clear()
         rslts.extend(await asyncio.gather(*tasks))
         flat_results = [item for sublist in rslts if sublist is not None for item in sublist if item is not None]
@@ -343,16 +341,20 @@ async def main():
                 if not any(url.endswith(end) for end in skip_extensions):
                     filtered_results.append(url)
         global_urls.extend(filtered_results)
-        print(len(global_urls), "url's were found. For each of them keywords were recorded in the database.")
         flat_results.clear()
         filtered_results.clear()
         if len(global_urls) == 0:
             print("No url's were found.")
             quit(0)
 
-input_thread1 = threading.Thread(target=input_reader, daemon=True)
-input_thread = threading.Thread(target=user_input_handler, daemon=True)
-flag_thread = threading.Thread(target=reset_flag, daemon=True)
+def start_threads():
+    input_thread = threading.Thread(target=user_input_handler, daemon=True)
+    flag_thread = threading.Thread(target=reset_flag, daemon=True)
+
+    print("Starting threads...")
+    input_thread.start()
+    flag_thread.start()
+    print("Threads started.")
 
 if __name__ == '__main__':
     while True:
@@ -374,6 +376,8 @@ if __name__ == '__main__':
                             print('Enter port:')
                             port = input()
                             if len(port) != 0:
+                                print('Enter the name of the table which you want to write data:')
+                                table = input()
                                 global_urls.append(first_url)
                                 db_config = {
                                     'dbname': db_name,
@@ -382,22 +386,20 @@ if __name__ == '__main__':
                                     'host': host,
                                     'port': port
                                 }
+                                SQL_INSERT_K = f'INSERT INTO {table} (url, keyword) VALUES (%s, %s)'
+                                start_threads()
                                 while True:
                                     try:
                                         connection = psycopg2.connect(**db_config)
                                         cursor = connection.cursor()
                                         print("Successfully connected to the database.")
                                         print('*' * len("Successfully connected to the database."))
-                                        asyncio.run(main())
-                                        input_thread1.start()
-                                        input_thread.start()
-                                        flag_thread.start()
+                                        asyncio.run(main(SQL_INSERT_K))
                                         break
                                     except Exception as error:
                                         last_error = error
                                         print('Failed connection to your database. Сheck the data you have entered:')
                                         print(db_config)
-                                        print('If you want to change it - print "changedata"/If you want to check error - print "info".')
                             else:
                                 print("Port cannot be empty.")
                         else:
